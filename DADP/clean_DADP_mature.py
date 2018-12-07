c2ef7b3c8eef3f48c28edd3534f767938d84581d
Chenkai Li <marklck0428@163.com>
2018-12-06 16:42:26 -0800

    Add files via upload

---
+++ DADP/clean_DADP_mature.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 15:21:01 2018
+
+@author: cli
+"""
+
+# In[]:
+import pandas as pd
+data = pd.read_csv('/projects/btl/cli/AMP_prediction_updated/data/AMP/DADP/DADP_all.csv')
+print(data.head())
+
+# In[]:
+# 2571 original precursors+AMPs
+bioactive = []
+for i in range(len(data)):
+    exec('temp =' + data.bioactive[i])
+    bioactive.append(temp)
+    
+# In[]:
+# output all bioactive as mature AMPs
+# 3004 mature AMPs
+mature = []
+for i in range(len(bioactive)):
+    for j in range(len(bioactive[i])):
+        mature.append(bioactive[i][j])
+        
+# In[]:
+# remove duplicates
+# 1923 distinct mature AMPs
+mature_cleaned = list(set(mature))
+
+# In[]:
+# 
